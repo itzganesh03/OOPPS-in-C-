185562ef6a72f0e2d1ef47f6610bf4e1f496be41
itzganesh <modiganesh83@gmail.com>
2022-08-16 16:20:45 +0530

    Update_1st_Practical

---
+++ 1st_practical_create_class-and-object/main.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+
+class Hero{
+    
+    // Proparty of class
+    int Health;
+};
+
+int main() {
+    
+    //Object Created
+    Hero Ganesh;
+    
+    cout << "size :"  << sizeof(Ganesh) << endl;
+    
+    return(0);
+}
